4044c2a6a771f81d194229db94248de26c54b7a6
Moreira-Edu <ml.ri7u@gmail.com>
2025-11-12 16:08:06 -0300

    feat: services campaign

---
+++ internal/domain/campaign/services.go
@@ -0,0 +1,13 @@
+package campaign
+
+import "emailn/internal/contract"
+
+type Service struct {
+	Repository Repository
+}
+
+func (s *Service) Create(NewCampaign contract.NewCampaign) error {
+
+	return nil
+
+}
